9d92ed8a37dec786676d64a76a7abf9f9edac632
Rahul Choudhary <rahul0101rock@gmail.com>
2020-10-31 23:00:03 +0530

    used random forest regression

--- production/views.py
+++ production/views.py
@@ -5,45 +5,51 @@ from matplotlib import pylab
 plt.rc("font", size=14)
 import PIL, PIL.Image
 from io import StringIO
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import seaborn as sns
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 def ploting(request) :
-    data=pd.read_csv("/home/predictcrop/crop_predict/production/apy.csv", header=0)
+    data=pd.read_csv("/home/predictcrop/crop_predict/production/apy.csv")
     data=data.dropna()
     inp=str(request.GET['inp']).split('@')
     dis=inp[0]
     season=inp[1]
+    area_in=inp[2]
     s=list(data["Season"].unique())
     for x in s:
         if season.title() in x:
             sin=s.index(x)
-    d = data[data["District_Name"]==dis.upper()][data["Season"]==s[sin]]["Crop"].unique()
-    c=[]
-    p=[]
-    for crop in d:
-        c.append(crop)
-        a = data[data["Crop"]==crop][data["District_Name"]==dis.upper()][data["Season"]==s[sin]]["Area"]
-        y = data[data["Crop"]==crop][data["District_Name"]==dis.upper()][data["Season"]==s[sin]]["Production"]
-        reg=LinearRegression()
-        reg.fit(a.values.reshape(-1,1),y.values.reshape(-1,1))
-        coeff=(reg.coef_)
-        p.append(coeff[0][0])
+    data_cu=data[data["District_Name"]==dis.upper()][data["Season"]==s[sin]]
+    data1 = data_cu.drop(["State_Name","Crop_Year"],axis=1)
+    data_dum = pd.get_dummies(data1)
+    x = data_dum.drop("Production",axis=1)
+    y = data_dum[["Production"]]
+
+    model = RandomForestRegressor()
+    model.fit(x,y.values.ravel())
+
+    ch=pd.DataFrame()
+    for crop in list(data_cu["Crop"].unique()):
+        t=(x[x["Crop_{}".format(crop)]==1])[:1]
+        ch=pd.concat([ch,t])
+    ch["Area"]=area_in
+    predict=model.predict(ch)
+    crname=data.loc[ch.index]["Crop"]
+    crdata= {'Crop': list(crname),
+            'Production': list(predict)}
+    crpro = pd.DataFrame(crdata)
+    crpro=crpro.sort_values(by=['Production'], ascending=False)
+    print(crpro)
     fig=plt.figure()
     ax = fig.add_axes([0,0,1,1])
-    crname=[]
-    pr=p.copy()
-    pr.sort(reverse=True)
-    for x in pr:
-        crname.append(c[p.index(x)])
-    tstr='Approx Prodction in        District: '+dis.title()+'      Season: '+season.title()
+    tstr='Predicted Production in        District: '+dis.title()+'      Season: '+season.title()
     ax.set_title(tstr,fontsize=15)
-    ax.set_ylabel('Production', fontsize=14)
+    ax.set_ylabel('Production in Tones', fontsize=14)
     ax.set_xlabel('Crop', fontsize=13)
-    plot=ax.bar(crname[:5],pr[:5 ])
-    plt.savefig('static/plot.png', dpi=400, bbox_inches='tight')
+    ax.bar(list(crpro["Crop"])[:5], list(crpro["Production"])[:5])
+    plt.savefig('static/plot.png', bbox_inches='tight')
     response="""<html><head>
-    <meta http-equiv="refresh" content="2; URL=http://predictcrop.pythonanywhere.com/static/plot.png" />
+    <meta http-equiv="refresh" content="0; URL=http://predictcrop.pythonanywhere.com/static/plot.png" />
     </head></html>"""
     return HttpResponse(response)
